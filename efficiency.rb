44b60fa48a987ee5f18ca1aff2498f01d67d2b6d
nctiggy <nctiggy@gmail.com>
2016-02-07 22:32:58 -0800

    Fixed some syntax errors, no biggie

--- efficiency.rb
+++ efficiency.rb
@@ -1,6 +1,7 @@
 require "rest_client"
 require "json"
 require "base64"
+require "pry-byebug"
 
 ########################
 # Method: API GET Method
@@ -24,18 +25,20 @@ def readSettings(file)
 	JSON.parse(settings)
 end
 
-config = readSettings(settings.json)
+config = readSettings('settings.json')
 config['xms'].each do |xms|
-	auth = Base64.strict_encode64("#{xms['user']}:#{xms['password']}")
+	auth = "Basic #{Base64.strict_encode64("#{xms['user']}:#{xms['password']}")}"
 	cluster_list = rest_get("https://#{xms['xms_ip']}/api/json/v2/types/clusters", auth)
 	cluster_list['clusters'].each do |cluster|
-		logical_space = ''
+		logical_space = 0
+		physical_space = 0
 		cluster_details = rest_get(cluster['href'], auth)
-		physical_space = cluster_details['content']['ud-ssd-space-in-use']
+		physical_space = cluster_details['content']['ud-ssd-space-in-use'].to_i
 		volume_list = rest_get("https://#{xms['xms_ip']}/api/json/v2/types/volumes", auth)
 		volume_list['volumes'].each do |volume|
-			logical_space += volume['content']['logical-space-in-use'] if volume['content']['sys-id'] == [cluster_details['content']['sys-id']
+			volume_details = rest_get(volume['href'], auth)
+			logical_space += volume_details['content']['logical-space-in-use'].to_i if volume_details['content']['sys-id'] == cluster_details['content']['sys-id']
 		end
-		puts "#{cluster_details['content']['sys-id'][1]} Data Reducation Rate:" + logical_space/physcial_space/1024/1024/1024
+		puts "#{cluster_details['content']['sys-id'][1]} Data Reducation Rate: #{logical_space/physical_space} to 1"
 	end
 end
